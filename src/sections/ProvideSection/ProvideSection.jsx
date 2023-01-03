937478b6f0698df1d2cb94fd03637c070d384400
Asker <kotsevasker0@gmail.com>
2023-01-03 15:36:08 +0300

    change the border

--- src/sections/ProvideSection/ProvideSection.jsx
+++ src/sections/ProvideSection/ProvideSection.jsx
@@ -19,7 +19,7 @@ const ProvideSection = () => {
             <p>Risus commodo id odio turpis pharetra elementum. Pulvinar porta porta feugiat<br/>
              scelerisque in elit. Morbi rhoncus, tellus, eros consequat magna semper orci a tincidunt. </p>
         </header>
-         <section>
+         <section className={style.section}>
                   {
                     card.map((el)=>{
                         return <CardTeams bg={"white"} image={el.image} title={el.title} text={el.text}/>
